09f0c8059bad2877e443296a15c3526d08880b70
Loic Jouhans <loic.jouhans@yahoo.fr>
2022-07-18 22:11:36 +0200

    Complete the last steps (delete + sort)

--- src/views/Challenge2.vue
+++ src/views/Challenge2.vue
@@ -1,18 +1,33 @@
 <template>
   <input type="number" v-model="sum" />
-  <br /><br />
   <button @click="addingNumber">Increment</button>
   <button @click="subtractNumber">Decrement</button>
   <button @click="clear">Clear</button>
+  <br /><br />
   <button type="submit" @click="submitForm">Submit</button>
+  <button @click="remove">Remove number</button>
+
   <div class="resul">
     <h2>added numbers</h2>
     <p id="error-msg"></p>
+    <p id="error-msg-delete"></p>
     <!-- eslint-disable-next-line vue/require-v-for-key -->
     <li v-for="item in items">
       {{ item }}
     </li>
   </div>
+
+  <fieldset>
+    <legend>Select the sort order:</legend>
+    <div id="v-model-checkbox" class="demo">
+      <input name="sort" type="radio" id="increasing" @click="increasingSort" />
+      <label for="checkbox">increasing</label>
+    </div>
+    <div id="v-model-checkbox" class="demo">
+      <input name="sort" type="radio" id="decreasing" @click="decreasingSort" />
+      <label for="checkbox">decreasing</label>
+    </div>
+  </fieldset>
 </template>
 
 <script>
@@ -22,10 +37,10 @@
  * 2) (optionel) ajout de la combobox qui mirror la liste de la div principale
  * 3) ajout du bouton de suppression ✅
  * 4) ajout de la sécurité sur ajout de doublon + affichage d'un message d'erreur dans la zone dédiée ✅
- * 5) ajout de la sécurité sur suppression d'élément absent + affichage du message d'erreur associé
+ * 5) ajout de la sécurité sur suppression d'élément absent + affichage du message d'erreur associé ✅
  * 6) intégration du tri par ordre croissant à l'insertion ✅
- * 7) intégration du tri par ordre décroissant, sélection par un booléen hardcodé
- * 8) ajout des boutons radio pour passer d'un tri à l'autre.
+ * 7) intégration du tri par ordre décroissant, sélection par un booléen hardcodé ✅
+ * 8) ajout des boutons radio pour passer d'un tri à l'autre.✅
  */
 import { defineComponent, ref } from "vue";
 
@@ -36,6 +51,7 @@ export default defineComponent({
     let items = ref([]);
     let sum = ref(0);
     let result = null;
+    let checked = false;
 
     function addingNumber() {
       sum.value++;
@@ -51,7 +67,6 @@ export default defineComponent({
 
     function submitForm() {
       result = sum.value;
-      // Voir comment faire avec la syntaxe VueJS (v-if/v-else)
       let errorMsg = document.getElementById("error-msg");
       if (items.value.includes(result)) {
         errorMsg.innerHTML = "This number already exists !";
@@ -61,17 +76,57 @@ export default defineComponent({
         // la partie (a, b) => a - b permet de faire une comparaison
         // et de trier de la bonne manière 1 & 10
         // https://medium.com/coding-at-dawn/how-to-sort-an-array-numerically-in-javascript-2b22710e3958#:~:text=In%20order%20to%20sort%20a,difference%20between%20the%20two%20numbers
-        items.value.sort((a, b) => a - b);
+        // items.value.sort((a, b) => a - b);
         errorMsg.innerHTML = "";
       }
     }
 
+    function remove() {
+      result = sum.value;
+      let errorMsg = document.getElementById("error-msg-delete");
+      let notIncludesNum = !items.value.includes(result);
+      for (let i = 0; i < items.value.length; i++) {
+        if (items.value[i] === result) {
+          items.value.splice(i, 1);
+          i--;
+        }
+      }
+
+      if (notIncludesNum) {
+        errorMsg.innerHTML = "this number has already been removed!";
+      }
+    }
+
+    function increasingSort() {
+      let checked = true;
+      // Si bouton croissant est coché alors tri croissant
+      if (checked === true) {
+        items.value.sort((a, b) => a - b);
+        console.log("ordre croissant");
+      }
+    }
+
+    function decreasingSort() {
+      let checked = false;
+      // Sinon (autre bouton coché) tri décroissant
+      if (checked === false) {
+        items.value.sort((a, b) => b - a);
+        console.log("ordre décroissant");
+      }
+    }
+
     return {
+      // Methods
+      increasingSort,
+      decreasingSort,
       subtractNumber,
       addingNumber,
       submitForm,
+      remove,
       clear,
 
+      // Data
+      checked,
       items,
       sum,
     };
@@ -83,5 +138,12 @@ export default defineComponent({
 .challenge2 {
   display: flex;
   justify-content: center;
+  label {
+    font: 1rem "Fira Sans", sans-serif;
+  }
+
+  input {
+    margin: 0.4rem;
+  }
 }
 </style>
